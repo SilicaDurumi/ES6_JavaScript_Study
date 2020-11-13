f75094a883a4e6bdd44497471b553627248cdcd9
Camille Neppy <dorumi714@gmail.com>
2020-11-13 07:56:30 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,202 @@
+// console.log("Im working JS");
+// const a = 221; let b = a-5;
+// console.log(b)
+// b = 0;
+// var c = 10;
+// c = 0;
+//let = can change, const = const = cant change, var = can
+// console.log(b, c);
+
+// const what = "TEST";
+// console.log(what);
+
+// const booleantest = true;
+// console.log(booleantest);
+
+// const floattest = 44.4;
+// console.log(floattest);
+
+// const monday = "Mon";
+// const tue = "Tue";
+// const wed = "Wed";
+// const thu = "Thu";
+// const fri = "Fri";
+
+// const daysOfWeek = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun", true,54];
+
+// console.log(monday, tue, wed, thu, fri);
+// console.log(daysOfWeek);
+
+// const testinfo = {
+//   name : "TEST",
+//   age : 1,
+//   gender:"Male",
+//   isHandsome:true,
+//   abc: ["ABC","BCD","CDF"],
+//   bcd: [
+//     {
+//       name : "AAA", 
+//       fatty:false
+//     },
+//     {
+//       name: "BBB",
+//       fatty:true
+//     }
+//   ]
+// };
+
+// console.log(testinfo.bcd[0].name);
+
+// console.log(testinfo.bcd[1]);
+
+// testinfo.gender = "Female";
+
+// console.log(testinfo.gender);
+
+// console.log(testinfo);
+
+// Console > (testinfo.name, 
+// testinfo.age, 
+// testinfo.gender, 
+// testinfo.abc, 
+// testinfo.bcd)
+
+// const hamaInfo = {
+// name : "hama",
+// age : 3,
+// gender:"Male",
+// favMovies : ["Along the gods", "Oldboy", "LOTR"] //객체안의 배열
+// ,
+// favFood: [
+//   [{name:"Kimchi"}, {fatty: false}],
+//   [{name:"Cheese burger"}, {fatty:true}]
+// ] //객체 안의 2차원 객체배열
+// }
+
+// console.log(hamaInfo.favFood[0][0]);
+// console.log(hamaInfo.favFood[0][1]);
+// console.log(hamaInfo.favFood[1][0]);
+// console.log(hamaInfo.favFood[1][1]);
+
+// console.log(console);
+// console.error = "TESTERROR";
+// console.log(console.error);
+// console.log('Hello Liyn');
+
+// function sayHello(name, age){
+//   return `Hello ${name} you are ${age} years old`;
+// }
+
+// const greeting = sayHello("ABC",99);
+// console.log(greeting);
+
+// console.log(sayHello("TEST", 15));
+
+// const calculator = {
+//   plus : function (number1, number2){return number1+number2;},
+//   minus : function(number1, number2){return number1-number2},
+//   mux : function(number1, number2){return number1*number2},
+//   div : function(number1, number2){return number1/number2},
+//   remain : function(number1, number2){return number1%number2},
+//   root : function(number){return number**0.5 ;}
+// }
+
+// const plus = calculator.plus(5,5);
+// console.log(plus);
+
+// const minus = calculator.minus(5,1);
+// console.log(minus);
+
+// const mux = calculator.mux(10,20);
+// console.log(mux);
+
+// const div = calculator.div(10,2);
+// console.log(div);
+
+// const remain = calculator.remain(9,2);
+// console.log(remain);
+
+// const root = calculator.root(2);
+// console.log(root);
+
+// const title = document.getElementById("title");
+// const title2 = document.querySelector("#title");
+
+// console.log(title);
+// console.log(title2);
+
+// console.log(title);
+// console.error("errorTest");
+
+// title.innerHTML = "TEST FROM JAVASCRIPT";
+// title.style.color = "olive";
+
+// document.title = "ABC";
+
+// function handleClick() {
+//   title.style.color = "black";
+// }
+
+// title.addEventListener("click", handleClick);
+// if('0' === 0){
+//   console.log('hi');
+// }
+// else if("10"==="11"){
+//   console.log("lalala");
+// }else{
+//   console.log("ho");
+// }
+
+// const lalala='lalala';
+
+// if (20>5 || "test"=="test"){
+//   console.log("yes");
+// }else{
+//   console.log("no");
+// }
+    
+// const test = prompt("Old");
+
+// if(test >=18&& test <21){ 
+//   console.log("YES");
+// }else if(test >= 21) {
+//   console.log("YYEESS");
+// }else{
+//   console.log("NO");
+// }
+
+// const title = document.querySelector("#title");
+
+// const CLICKED_CLASS = "clicked";
+
+// function handleClick(){
+//   title.classList.toggle(CLICKED_CLASS);
+// }
+
+// function init(){
+//   title.addEventListener("click", handleClick);
+// }
+
+// init();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
